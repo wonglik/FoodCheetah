6c35b01f84088e4664f40278eab6d5eff41cf1fd
Wong Lik <81523243+wonglik@users.noreply.github.com>
2021-06-12 23:20:56 +0800

    12/6 23:20 Wl (until part 16)

--- app/src/main/java/com/wonglik/foodcheetah/CustomerFoodPanel_BottomNavigation.java
+++ app/src/main/java/com/wonglik/foodcheetah/CustomerFoodPanel_BottomNavigation.java
@@ -1,14 +1,72 @@
 package com.wonglik.foodcheetah;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class CustomerFoodPanel_BottomNavigation extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.wonglik.foodcheetah.chefFoodPanel.ChefHomeFragment;
+import com.wonglik.foodcheetah.chefFoodPanel.ChefOrderFragment;
+import com.wonglik.foodcheetah.chefFoodPanel.ChefPendingOrderFragment;
+import com.wonglik.foodcheetah.chefFoodPanel.ChefProfileFragment;
+import com.wonglik.foodcheetah.customerFoodPanel.CustomerCartFragment;
+import com.wonglik.foodcheetah.customerFoodPanel.CustomerHomeFragment;
+import com.wonglik.foodcheetah.customerFoodPanel.CustomerOrdersFragment;
+import com.wonglik.foodcheetah.customerFoodPanel.CustomerProfileFragment;
+import com.wonglik.foodcheetah.customerFoodPanel.CustomerTrackFragment;
+
+public class CustomerFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_food_panel_bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.cust_Home:
+                fragment=new CustomerHomeFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cart:
+                fragment=new CustomerCartFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cust_profile:
+                fragment=new CustomerProfileFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.Cust_order:
+                fragment=new CustomerOrdersFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.track:
+                fragment=new CustomerTrackFragment();
+                break;
+        }
+        return loadcheffragment(fragment);
+
+    }
+
+    private boolean loadcheffragment(Fragment fragment) {
+
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
